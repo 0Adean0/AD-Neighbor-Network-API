78f7443f6f9343f59dd60d6f9c40d2a6f992b314
MuheCa <119535228+0Adean0@users.noreply.github.com>
2023-05-16 08:24:57 -0400

    completed userRoutes.js near similar to thoughtRoutes in structure and content

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const{
+    getAllUsers,
+    getOneUserId,
+    generateUser,
+    modifyUserId,
+    deleteUserId,
+    generateNeighbor,
+    deleteNeighbor
+} = require("../../controllers/userController");
+
+router
+.route("/")
+.get(getAllUsers)
+.post(generateUser);
+router
+.route('/:userId')
+.get(getOneUserId)
+.put(modifyUserId)
+.delete(deleteUserId);
+router
+.route("/:userId/neighbor/:neighborId")
+.post(generateNeighbor)
+.delete(deleteNeighbor);
+
+module.exports = router
